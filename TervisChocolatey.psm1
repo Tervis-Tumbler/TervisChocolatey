a176119e50e6fff21e656d68f038e5f84a931106
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-07 15:16:45 -0400

    Initial commit

---
+++ TervisChocolatey.psm1
@@ -0,0 +1,20 @@
+﻿function New-TervisChocolateyPackage {
+    param (
+        $PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0],
+        $PackageName,
+        $URL
+    )
+
+    choco new $PackageName --outputdirectory "$PowerShellModulesPath\chocolateyautomaticpackages\Static" maintainername="TervisIT" maintainerrepo="https://github.com/Tervis-Tumbler/chocolateyautomaticpackages/tree/master/Static/$PackageName" url="$URL"
+    
+}
+
+function Invoke-TervisChocolateyPackPackage {
+    param (
+        $PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0],
+        $PackageName
+    )
+    &choco.exe pack $PowerShellModulesPath\chocolateyautomaticpackages\Static\$PackageName\$PackageName.nuspec --outputdirectory "\\tervis.prv\applications\Chocolatey"
+}
+
+#New-TervisChocolateyPackage -PackageName iVMS-4200 -URL "http://oversea-download.hikvision.com/uploadfile/USA/Software/iVMS-4200v2.5.0.5Download_Package_contains_Lite_&_Full_versions.zip"
