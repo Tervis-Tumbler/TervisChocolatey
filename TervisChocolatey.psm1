15b7f09de7271edae1abe9c61333da3b37a65380
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-07 16:17:40 -0400

    Add install function

--- TervisChocolatey.psm1
+++ TervisChocolatey.psm1
@@ -1,9 +1,9 @@
 ﻿function New-TervisChocolateyPackage {
     param (
         $PowerShellModulesPath = ($ENV:PSModulepath -split ";")[0],
-        $PackageName,
-        $URL,
-        $Version
+        [Parameter(Mandatory)]$PackageName,
+        [Parameter(Mandatory)]$URL,
+        [Parameter(Mandatory)]$Version
     )
 
     choco new $PackageName --outputdirectory "$PowerShellModulesPath\chocolateyautomaticpackages\Static" maintainername="TervisIT" maintainerrepo="https://github.com/Tervis-Tumbler/chocolateyautomaticpackages/tree/master/Static/$PackageName" url="$URL" packageversion="$Version"
@@ -20,3 +20,11 @@ function Invoke-TervisChocolateyPackPackage {
 }
 
 #New-TervisChocolateyPackage -PackageName iVMS-4200 -URL "http://oversea-download.hikvision.com/uploadfile/USA/Software/iVMS-4200v2.5.0.5Download_Package_contains_Lite_&_Full_versions.zip"
+
+function Install-TervisChocolateyPackageInstall {
+    param (
+        [Parameter(Mandatory)]$PackageName
+    )
+
+    choco install $PackageName --source="\\tervis.prv\applications\Chocolatey" -y --allowemptychecksum
+}
